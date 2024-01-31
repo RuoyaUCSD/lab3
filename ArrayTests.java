38ba3795700ac60ebac6dbc469f8a00e659338a0
Ruoya <156246456+RuoyaUCSD@users.noreply.github.com>
2024-01-31 12:46:42 -0800

    Fix reversers; add .gitignore, test.sh

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,37 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
+	@Test 
+	public void testReverseInPlaceLen3() {
+    int[] input1 = { 3, 5, 4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 4, 5, 3 }, input1);
+	}
+	@Test 
+	public void testReverseInPlaceLen4() {
+    int[] input1 = { -1, 3, 0, -5 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ -5, 0, 3, -1 }, input1);
+	}
+	public void testReverseInPlaceLen0() {
+    int[] input1 = {};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{}, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+  @Test
+  public void testReversedLen3() {
+    int[] input1 = {1,5,4 };
+    assertArrayEquals(new int[]{ 4,5,1}, ArrayExamples.reversed(input1));
+  }
+  @Test
+  public void testReversedGivesNewArr() {
+    int[] input1 = { };
+    assertNotEquals(input1, ArrayExamples.reversed(input1));
   }
 }
